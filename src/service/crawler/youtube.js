b260c99cde8ed17041c4b2d912430663d40dd577
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-08-09 03:04:06 -0300

    feat: change yoututbe crawler to get videoId(url)

--- src/service/crawler/youtube.js
+++ src/service/crawler/youtube.js
@@ -1,29 +1,28 @@
-var https = require('https');
-var cheerio = require('cheerio');
-var Promise = require('bluebird');
+const fetch = require("node-fetch");
+const url = "https://www.youtube.com/c/SpaceX/videos"
 
-module.exports = function (query) {
-  const url = `https://www.youtube.com/user/univesptv/videos`
-  return new Promise(function (resolve, reject) {
-    try {
-      https.get(url, function(res) {
-        var data = '';
-        var results = [];
+const getData = async url => {
+  try {
+        const response = await fetch(url);
+        const content = await response.text()
+        const preJson = content.split("window[\"ytInitialData\"] = ")[1].split(";")[0]
+        const json = JSON.parse(preJson);
+        const tabs = json.contents.twoColumnBrowseResultsRenderer.tabs;
+        tabs.map(tab => {
+            const tabContent = tab.tabRenderer && tab.tabRenderer.content;
+            if(tabContent && tabContent.sectionListRenderer){
+                  tabContent.sectionListRenderer.contents.map(content => {
+                      const itens = content.itemSectionRenderer.contents[0].gridRenderer.items;
+                      itens.map(item => {
+                          console.log(item.gridVideoRenderer.navigationEndpoint.watchEndpoint.videoId)
+                      })
+                 })
 
-        res.on('data', function(chunk) {
-          data += chunk;
-        });
-
-        res.on('end', function() {
-          var $ = cheerio.load(data);
-          var videos = $('.ytd-grid-renderer');
-            console.log(videos);
-
-          resolve(results);
-        });
-      });
-    } catch (err) {
-      reject(err);
-    }
-  });
+            }
+        })
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+getData(url);
