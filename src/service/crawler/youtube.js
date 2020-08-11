450b721786b9b053735874f1b06f588d9c25b358
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-08-10 21:18:11 -0300

    wip in youtube converter

--- src/service/crawler/youtube.js
+++ src/service/crawler/youtube.js
@@ -1,28 +1,46 @@
 const fetch = require("node-fetch");
-const url = "https://www.youtube.com/c/SpaceX/videos"
 
-const getData = async url => {
+
+
+const getChannelJson = async channel => {
+ const url = `https://www.youtube.com/c/${channel}/videos`
   try {
         const response = await fetch(url);
         const content = await response.text()
         const preJson = content.split("window[\"ytInitialData\"] = ")[1].split(";")[0]
-        const json = JSON.parse(preJson);
-        const tabs = json.contents.twoColumnBrowseResultsRenderer.tabs;
-        tabs.map(tab => {
-            const tabContent = tab.tabRenderer && tab.tabRenderer.content;
-            if(tabContent && tabContent.sectionListRenderer){
-                  tabContent.sectionListRenderer.contents.map(content => {
-                      const itens = content.itemSectionRenderer.contents[0].gridRenderer.items;
-                      itens.map(item => {
-                          console.log(item.gridVideoRenderer.navigationEndpoint.watchEndpoint.videoId)
-                      })
-                 })
-
-            }
-        })
+        const json = JSON.parse(preJson);        
+        return json.contents.twoColumnBrowseResultsRenderer.tabs;
   } catch (error) {
     console.log(error);
   }
 };
 
-getData(url);
+
+const tabsConvertToGridRenderer = tabs => tabs.filter(tab => tab.tabRenderer && tab.tabRenderer.content)[0]
+            .tabRenderer
+            .content
+            .sectionListRenderer
+            .contents[0]
+            .itemSectionRenderer
+            .contents[0]
+            .gridRenderer
+            .items;
+
+
+
+const getData = async channel => {
+  const tabs = await getChannelJson(channel);
+  const items = await tabsConvertToGridRenderer(tabs);
+  return items.reduce( (acc, { gridVideoRenderer }) => {
+      return {
+          "videoId" : gridVideoRenderer.videoId,
+          "thumbnail" : gridVideoRenderer.thumbnail.thumbnails,
+          "title" : gridVideoRenderer.title.simpleText,
+          "publishedTimeText" : gridVideoRenderer.publishedTimeText.simpleText,
+          "viewCountText" : gridVideoRenderer.viewCountText.simpleText
+      }
+   },[])
+
+}
+
+console.log(getData("spacex"));
