595ca989c954e8f43eac17aa51e22d8b6d61afe2
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-08-22 21:12:24 -0300

    feat: add profile in twith channel

--- src/services/twitch/connector/twitch.connector.ts
+++ src/services/twitch/connector/twitch.connector.ts
@@ -1,7 +1,22 @@
 
 import {getFromUrl} from "../../shared"
 const urlBase = "https://gql.twitch.tv/gql"
-const _getBody = ({requestID, hash, game}) => JSON.stringify([
+const _getBody = ({hashChanels, hashBanner, requestID, game}) => JSON.stringify([
+   {
+        "operationName": "Directory_DirectoryBanner",
+        "variables": {
+            "name": game,
+            "sortTypeIsRecency": false,
+        },
+        "extensions": {
+            "persistedQuery": {
+                "version": 1,
+                "sha256Hash": hashBanner, 
+            }
+        },
+
+    },   
+
     {
         "operationName": "DirectoryPage_Game",
         "variables": {
@@ -23,23 +38,29 @@ const _getBody = ({requestID, hash, game}) => JSON.stringify([
         "extensions": {
             "persistedQuery": {
                 "version": 1,
-                "sha256Hash": hash            }
+                "sha256Hash": hashChanels
+            }
         }
     }
 ]);
 
 const GetDataChannelText = async (game:string) => {
-   const hash = "5feb6766dc5d70b33ae9a37cda21e1cd7674187cb74f84b4dd3eb69086d9489c"
-   const requestID = "JIRA-VXP-2397"
-   const headers = {
-        headers: { 
-            'Content-Type': 'application/json',
-            'x-requested-with': 'XMLHttpRequest',
-            'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
-        },
-        method: 'POST',
-        body: _getBody({hash, requestID, game })     
+   const payload = {
+       "hashChanels" : "5feb6766dc5d70b33ae9a37cda21e1cd7674187cb74f84b4dd3eb69086d9489c",
+       "hashBanner" : "a64b0348103e054cbdb20c58de5fc05160da3f86c37c80263d7e6282f2577f54",
+       "requestID" : "JIRA-VXP-2397",
+       "game": game
    }
+   const headers = {
+        "headers": { 
+                'Content-Type': 'application/json',
+                'x-requested-with': 'XMLHttpRequest',
+                'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
+        },
+        "method" : 'POST',
+        "body" : _getBody(payload)  
+   }
+   
    const bodyText = await getFromUrl(urlBase, headers);
    return bodyText;
 }
