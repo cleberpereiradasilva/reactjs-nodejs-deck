1d8294e6c450a092e7202fd2700674bcaddce4a5
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-08-27 17:26:18 -0300

    feat: implements new Request in youtube

--- src/services/youtube/connector/youtube.connector.ts
+++ src/services/youtube/connector/youtube.connector.ts
@@ -1,19 +1,36 @@
-import {getFromUrl} from "../../shared";
+import RequestInterface from "../../request/RequestInterface";
+import {FetchRequest} from "../../request";
+import {RequestPayload} from "../../shared/model/WebPayload";
 
 const urlChannel = (channel: string) => `https://www.youtube.com/c/${channel}/videos`
 const urlVideo = (videoId: string) => `https://www.youtube.com/watch?v=${videoId}`
+const Request:RequestInterface = new FetchRequest();
+
 
 const GetChannelText = async (channel : string) => {
-    const url = urlChannel(channel);
-    return await getFromUrl(url);
+    const payload: RequestPayload = {
+        url: urlChannel(channel),        
+        method: 'GET',
+        headers: { 
+                'Content-Type': 'application/html',
+                'x-requested-with': 'XMLHttpRequest' 
+        }                
+    }
+    return await Request.execute(payload);
 };
 
 const GetVideoText = async (videoId : string) => {
-    const url = urlVideo(videoId);
-    return await getFromUrl(url);
+    const payload: RequestPayload = {
+        url: urlVideo(videoId),        
+        method: 'GET',
+        headers: { 
+                'Content-Type': 'application/html',
+                'x-requested-with': 'XMLHttpRequest' 
+        }                
+    }
+    return await Request.execute(payload);
 };
 
 export { GetChannelText, GetVideoText}
-
 
 
