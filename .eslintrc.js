5e538a1a5dcf274d8d3741a1d89eaf0bae6ad799
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-08-29 05:19:09 -0300

    chore: test lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   ],
   rules: {
       "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/camelcase" : "off"
+      "@typescript-eslint/camelcase": "off"
   },
 
   overrides: [
