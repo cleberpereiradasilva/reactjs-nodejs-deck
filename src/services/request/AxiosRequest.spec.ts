88522c6aeb749e87dcf7f287dd94b078f5a0d810
Cleber Silva <cleber_pesilva@yahoo.com.br>
2020-08-30 23:47:41 -0300

    test: add AxsiosRequest test

---
+++ src/services/request/AxiosRequest.spec.ts
@@ -0,0 +1,23 @@
+import {AxiosRequest} from ".";
+import {RequestPayload} from "../shared/model/WebPayload";
+import RequestInterface from "./RequestInterface";
+
+
+class SutAxiosRequest implements RequestInterface{
+        async execute(playload: RequestPayload): Promise<string>{
+            return new Promise(resolve => resolve("valid_content"));
+        }
+ }
+
+
+describe("AxiosRequest", () => {
+    const sutAxiosRequest: RequestInterface = new SutAxiosRequest();
+    const execute = jest.spyOn(sutAxiosRequest, "execute");
+    test("Testing execute", () => {
+        sutAxiosRequest.execute(null);
+        expect(execute).toBeCalled();
+        expect(execute).toBeCalledWith(null);
+
+    })
+
+})
